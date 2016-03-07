b28dc144d956f13fa8db8b3624cec77f6000f8a5
loboy <loboyoh@pchome.com.tw>
2016-03-07 21:40:35 +0800

    Application2_Submit_to Change_TextView
    Application2

---
+++ app/src/main/java/com/example/loboy/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.loboy.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView1 = (TextView) findViewById(R.id.textView);
+    }
+
+    public void submit(View view)
+    {
+        textView1.setText("For Test"); //.SetText("For Test");
+
+    }
+}
